3d91cc3402693ff44da716b69ebfdbcbb9e75ba9
ggg-geyi <1131145@qq.com>
2017-08-22 17:41:47 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+var addon = require('./build/Release/addon');
+console.log(addon.hello(0));
+console.log(addon.hello(1));
+console.log(addon.hello(2));
+console.log(addon.hello(3));
+console.log(addon.hello(4));
+console.log(addon.hello(5));
+console.log(addon.hello(6));
